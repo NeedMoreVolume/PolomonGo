f18da11827b5591c8f1a1a66329d9692a7cee7bf
NeedMoreVolume <pivalizza.d@comcast.net>
2019-03-12 19:43:53 -0500

    Updated flags

--- main.go
+++ main.go
@@ -13,34 +13,21 @@ import (
 
 func main() {
 	markets := [6]string{"btc_etc", "btc_eth", "btc_ltc", "btc_rep", "btc_xmr", "btc_xrp"}
-	dbFlagPtr := flag.String("db", "cassandra", "DB to operate on.")
-	actionFlagPtr := flag.String("action", "menu", "Action for CLI to take (update DB or menu)")
+	actionFlagPtr := flag.String("action", "menu", "Available actions: menu, update")
 	flag.Parse()
 	client, err := mongo.Connect(context.Background(), "mongodb://localhost:27017")
 	err = client.Ping(context.Background(), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	switch *actionFlagPtr {
-	case "update":
-		switch *dbFlagPtr {
-		case "cassandra":
-			for _, market := range markets {
-				poloniex.PutDataInCassandra(&market)
-			}
-			fmt.Println("Update complete!")
-			return
-		case "mongo":
-			for _, market := range markets {
-				poloniex.PutDataInMongo(client, time.Now().Unix(), &market)
-			}
-			fmt.Println("Update complete!")
-			return
-		case "":
-			fmt.Println("Please enter a db to update.")
-		}
-	case "menu":
-		menu.MenuHandler(client)
-	}
+	if err != nil {log.Fatal(err)}
+  if *actionFlagPtr == "menu" {
+    menu.MenuHandler(client)
+  } else if *actionFlagPtr == "update" {
+    for _, market := range markets {
+      poloniex.PutDataInMongo(client, time.Now().Unix(), &market)
+      poloniex.PutDataInCassandra(&market)
+    }
+    fmt.Println("All DB updates complete!")
+  } else {
+    fmt.Println("Please use -h for help.")
+  }
 	return
 }
